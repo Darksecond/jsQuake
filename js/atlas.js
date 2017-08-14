0aaaedd7035afa81933a5d7d40e859024a309aad
Tim Peters <tim@wirelab.nl>
2017-08-14 15:46:24 +0200

    Add basic texture atlas

---
+++ js/atlas.js
@@ -0,0 +1,83 @@
+define(function(){
+  "use strict";
+
+  function Atlas(width, height, gl) {
+    if (!(this instanceof Atlas)) {
+      throw new TypeError("Atlas constructor cannot be called as a function.");
+    }
+
+    this.gl = gl;
+    this.freeRects = [{xy: [0,0], size: [width, height]}];
+    this.width = width;
+    this.height = height;
+
+    this.texture = this.gl.createTexture();
+    this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture);
+    this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGB,
+      width, height, 0, this.gl.RGB,
+      this.gl.UNSIGNED_BYTE, new Uint8Array(width*height*3).fill(0));
+
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.REPEAT);
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.REPEAT);
+  }
+
+  Atlas.prototype = {
+    constructor: Atlas,
+
+    allocate: function(width, height, data) {
+      var bestRect;
+      var bestScore = Infinity;
+      for(var i=0; i < this.freeRects.length; i++) {
+        var rect = this.freeRects[i];
+        var score = (rect.size[0]-width) + (rect.size[1]-height);
+        if(score == 0) {
+          bestRect = rect;
+          bestScore = score;
+          break;
+        } else if(score > 0 && score < bestScore && rect.size[0] >= width && rect.size[1] >= height) {
+            bestScore = score;
+            bestRect = rect;
+        }
+      }
+      if(bestRect) {
+        this.freeRects.splice(this.freeRects.indexOf(bestRect), 1);
+        var rect1 = {
+          xy: [bestRect.xy[0],bestRect.xy[1]+height],
+          size: [bestRect.size[0],bestRect.size[1]-height],
+        };
+        var rect2 = {
+          xy: [bestRect.xy[0]+width,bestRect.xy[1]],
+          size: [bestRect.size[0]-width,height],
+        };
+
+        if(rect1.size[0] > 0 && rect1.size[1] > 0) {
+          this.freeRects.push(rect1);
+        }
+        if(rect2.size[0] > 0 && rect2.size[1] > 0) {
+          this.freeRects.push(rect2);
+        }
+
+        if(data) {
+          this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture);
+          this.gl.texSubImage2D(this.gl.TEXTURE_2D,
+            0,
+            bestRect.xy[0],
+            bestRect.xy[1],
+            width,
+            height,
+            this.gl.RGB,
+            this.gl.UNSIGNED_BYTE,
+            data);
+        }
+        return {xy: bestRect.xy, size: [width, height]};
+      } else {
+        console.error('No fitting rect found');
+        return null;
+      }
+    },
+  };
+
+  return Atlas;
+});
--- js/bspRenderer.js
+++ js/bspRenderer.js
@@ -1,4 +1,4 @@
-define(['glMatrix'], function(GLM) {
+define(['glMatrix', 'atlas'], function(GLM, Atlas) {
   "use strict";
 
   function BSPRenderer(gl) {
@@ -77,6 +77,13 @@ define(['glMatrix'], function(GLM) {
       this.bspProgram.uniform.texture = this.gl.getUniformLocation(
         this.bspProgram,
         "texture");
+
+      this.bspProgram.uniform.texSize = this.gl.getUniformLocation(
+        this.bspProgram,
+        "texSize");
+      this.bspProgram.uniform.texOffset = this.gl.getUniformLocation(
+        this.bspProgram,
+        "texOffset");
     },
 
     //TODO Clean this up
@@ -129,23 +136,25 @@ define(['glMatrix'], function(GLM) {
         mip[i*3+1] = this.palette[idx*3+1];
         mip[i*3+2] = this.palette[idx*3+2];
       }
-      
-      var texture = this.gl.createTexture();
-      this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
-      this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGB,
-        surface.mipTex.width, surface.mipTex.height, 0, this.gl.RGB,
-        this.gl.UNSIGNED_BYTE, mip);
 
-      this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
-      this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);
-      this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.REPEAT);
-      this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.REPEAT);
-      return texture;
+      let rect = this.atlas.allocate(surface.mipTex.width, surface.mipTex.height, mip);
+      
+      return {
+        xy: [
+          rect.xy[0]/this.atlas.width,
+          rect.xy[1]/this.atlas.height,
+        ],
+        size: [
+          rect.size[0]/this.atlas.width,
+          rect.size[1]/this.atlas.height
+        ],
+      };
     },
 
     load: function(bsp, palette) {
       this.bsp = bsp;
       this.palette = new Uint8Array(palette);
+      this.atlas = new Atlas(2048, 2048, this.gl);
 
       this.loadVertices();
     },
@@ -178,8 +187,10 @@ define(['glMatrix'], function(GLM) {
         this.gl.vertexAttribPointer(this.bspProgram.attribute.uv, 2, this.gl.FLOAT, false, 5*4, 3*4);
 
         this.gl.activeTexture(this.gl.TEXTURE0);
-			  this.gl.bindTexture(this.gl.TEXTURE_2D, vbo.tex);
+			  this.gl.bindTexture(this.gl.TEXTURE_2D, this.atlas.texture);
 			  this.gl.uniform1i(this.bspProgram.uniform.texture, 0);
+        this.gl.uniform2fv(this.bspProgram.uniform.texSize, vbo.tex.size);
+        this.gl.uniform2fv(this.bspProgram.uniform.texOffset, vbo.tex.xy);
 
         this.gl.drawArrays(this.gl.TRIANGLES, 0, vbo.count);
       }
